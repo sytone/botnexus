26abedbbdae291ff3e2fe0a72ce63eb7a18b9223
larry-fox-lobster <264885659+larry-fox-lobster@users.noreply.github.com>
2026-05-18 10:08:50 -0700

    refactor(mobile): replace MobileState/MobileGatewayClient with Core services (#338)

--- src/extensions/BotNexus.Extensions.Channels.SignalR.BlazorClient.Mobile/Program.cs
+++ src/extensions/BotNexus.Extensions.Channels.SignalR.BlazorClient.Mobile/Program.cs
@@ -1,4 +1,4 @@
-using BotNexus.Extensions.Channels.SignalR.BlazorClient.Mobile.Services;
+using BotNexus.Extensions.Channels.SignalR.BlazorClient.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,7 +6,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<BotNexus.Extensions.Channels.SignalR.BlazorClient.Mobile.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Reuse service registrations from desktop client
-builder.Services.AddBotNexusMobileServices(builder.Configuration);
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<GatewayHubConnection>();
+builder.Services.AddScoped<IClientStateStore, ClientStateStore>();
+builder.Services.AddScoped<IGatewayRestClient, GatewayRestClient>();
+builder.Services.AddScoped<IGatewayEventHandler, GatewayEventHandler>();
+builder.Services.AddScoped<IAgentInteractionService, AgentInteractionService>();
+builder.Services.AddScoped<IPortalLoadService, PortalLoadService>();
 
 await builder.Build().RunAsync();
