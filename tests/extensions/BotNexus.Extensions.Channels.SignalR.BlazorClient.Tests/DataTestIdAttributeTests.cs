1378689ef019a4b6980c783650564119acaf48d8
BotNexus Test <botnexus@test.local>
2026-06-11 18:25:38 -0700

    fix(portal): include cron-assigned conversations in scheduled sidebar group
    Closes #1268

--- tests/extensions/BotNexus.Extensions.Channels.SignalR.BlazorClient.Tests/DataTestIdAttributeTests.cs
+++ tests/extensions/BotNexus.Extensions.Channels.SignalR.BlazorClient.Tests/DataTestIdAttributeTests.cs
@@ -45,6 +45,7 @@ public sealed class DataTestIdAttributeTests : IDisposable
         _ctx.Services.AddSingleton(Substitute.For<IChannelErrorReporter>());
         _ctx.Services.AddSingleton(http);
         _ctx.Services.AddSingleton(new ExtensionFeatureService(restClient));
+        _ctx.Services.AddSingleton(new CronApiClient(http));
         _ctx.JSInterop.Mode = JSRuntimeMode.Loose;
     }
 
